cdfa375e40faff5f94767c93c20791097e139a60
Vincent Theron <vptheron@gmail.com>
2014-08-18 11:13:11 -0400

    add eventSource trait, and use it to display changing altitude

--- src/main/scala/zzz/akka/avionics/Altimeter.scala
+++ src/main/scala/zzz/akka/avionics/Altimeter.scala
@@ -9,13 +9,17 @@ object Altimeter {
 
   case class RateChange(amount: Float)
 
+  case class AltitudeUpdate(altitude: Double)
+
   private val ceiling = 43000
   private val maxRateOfClimb = 5000
 
   private case object Tick
+
 }
 
-class Altimeter extends Actor with ActorLogging {
+class Altimeter
+  extends Actor with ActorLogging with EventSource {
 
   import Altimeter._
 
@@ -27,7 +31,7 @@ class Altimeter extends Actor with ActorLogging {
 
   private val ticker = context.system.scheduler.schedule(100.millis, 100.millis, self, Tick)
 
-  def receive = {
+  private def altimeterReceive: Receive = {
     case RateChange(amount) =>
       rateOfClimb = amount.min(1.0f).max(-1.0f) * maxRateOfClimb
       log.info(s"Altimeter changed rate of climb to $rateOfClimb")
@@ -36,7 +40,10 @@ class Altimeter extends Actor with ActorLogging {
       val tick = System.currentTimeMillis
       altitude = altitude + ((tick - lastTick) / 60000.0) * rateOfClimb
       lastTick = tick
+      sendEvent(AltitudeUpdate(altitude))
   }
+
+  def receive = eventSourceReceive orElse altimeterReceive
 
   override def postStop(): Unit = {
     ticker.cancel()
--- src/main/scala/zzz/akka/avionics/Plane.scala
+++ src/main/scala/zzz/akka/avionics/Plane.scala
@@ -12,13 +12,22 @@ class Plane extends Actor with ActorLogging {
 
   import Altimeter._
   import Plane._
+  import EventSource._
 
   val altimeter = context.actorOf(Props[Altimeter], "Altimeter")
   val controls = context.actorOf(Props(new ControlSurfaces(altimeter)), "ControlSurfaces")
+
+  override def preStart(): Unit ={
+    altimeter ! RegisterListener(self)
+  }
 
   def receive = {
     case GiveMeControl =>
       log.info("Plane giving control.")
       sender ! controls
+
+    case AltitudeUpdate(altitude) =>
+      log.info(s"Altitude is now: $altitude")
   }
+
 }
